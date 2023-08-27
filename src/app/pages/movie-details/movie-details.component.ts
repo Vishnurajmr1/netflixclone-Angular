0dbce5001cf4e5f4af6a77c2f559b50f2a485a24
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-08-28 02:15:28 +0530

    different categories added

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Movie } from 'src/app/interface/movie';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  constructor(
+    private service: MovieApiServiceService,
+    private router: ActivatedRoute
+  ) {}
+  movieDetails: any = [];
+  movieVideoResult: any = [];
+  movieCastDetails: any = [];
+  ngOnInit(): void {
+    let paramId: string = this.router.snapshot.paramMap.get('id')!;
+    this.getMovieDetails(paramId);
+    this.getVideo(paramId);
+    this.getMovieCast(paramId);
+  }
 
+  getMovieDetails(id: string) {
+    this.service.getMovieDetails(id).subscribe((result): void => {
+      console.log(result, 'getmoviedetatils#');
+      this.movieDetails = result;
+    });
+  }
+
+  getVideo(id: string) {
+    this.service.getMovieVideo(id).subscribe((result) => {
+      result.results.forEach((element: any) => {
+        if (element.type == 'Trailer' || element.type == 'Teaser') {
+          this.movieVideoResult = element.key;
+        }
+      });
+    });
+  }
+
+  getMovieCast(id: string) {
+    this.service.getMovieCast(id).subscribe((result) => {
+      this.movieCastDetails = result.cast;
+    });
+  }
 }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Movie } from 'src/app/interface/movie';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
 
+  ngOnInit(): void {}
+  searchResult: Movie[] = [];
+  searchForm = new FormGroup({
+    movieName: new FormControl(null),
+  });
+
+  submitForm() {
+    console.log(this.searchForm.value, 'searchForm#');
+    this.service.getSearchMovie(this.searchForm.value).subscribe((result) => {
+      console.log(result, 'searchMovie##');
+      this.searchResult = result.results;
+    });
+  }
 }
